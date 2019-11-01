4837e8659b6f4433147523d3bd11bec13ad32b5e
neil-sriv <neil.srivastava1@gmail.com>
2019-10-31 23:18:03 -0500

    database connection

--- my_app/lib/search_page.dart
+++ my_app/lib/search_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class SearchWidget extends StatefulWidget {
@@ -11,8 +12,64 @@ class _SearchState extends State<SearchWidget>{
    final Color color = Colors.white;
  @override
  Widget build(BuildContext context) {
-   return Container(
-     color: color,
+   return Scaffold(
+     body: _buildBody(context),
    );
  }
+
+ Widget _buildBody(BuildContext context) {
+ return StreamBuilder<QuerySnapshot>(
+   stream: Firestore.instance.collection('items').snapshots(),
+   builder: (context, snapshot) {
+     if (!snapshot.hasData) return LinearProgressIndicator();
+
+     return _buildList(context, snapshot.data.documents);
+   },
+ );
+}
+
+ Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+   return ListView(
+     padding: const EdgeInsets.only(top: 20.0),
+     children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+   );
+ }
+
+ Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+ final record = Record.fromSnapshot(data);
+
+   return Padding(
+     key: ValueKey(record.name),
+     padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+     child: Container(
+       decoration: BoxDecoration(
+         border: Border.all(color: Colors.grey),
+         borderRadius: BorderRadius.circular(5.0),
+       ),
+       child: ListTile(
+         title: Text(record.name),
+         trailing: Text(record.likes.toString()),
+         onTap: () => record.reference.updateData({'likes': FieldValue.increment(1)}),
+       ),
+     ),
+   );
+ }
+}
+
+class Record {
+ final String name;
+ final int likes;
+ final DocumentReference reference;
+
+ Record.fromMap(Map<String, dynamic> map, {this.reference})
+     : assert(map['Name'] != null),
+       assert(map['likes'] != null),
+       name = map['Name'],
+       likes = map['likes'];
+
+ Record.fromSnapshot(DocumentSnapshot snapshot)
+     : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+ @override
+ String toString() => "Record<$name:$likes>";
 }
