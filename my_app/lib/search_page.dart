9eaac28836519e41742e22c95697f2b3dfcbacc3
neil-sriv <neil.srivastava1@gmail.com>
2019-11-13 20:25:45 -0600

    dynamic search list

--- my_app/lib/search_page.dart
+++ my_app/lib/search_page.dart
@@ -1,75 +1,136 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 class SearchWidget extends StatefulWidget {
   @override
- State<StatefulWidget> createState() {
-  return _SearchState();
+  State<StatefulWidget> createState() {
+    return _SearchState();
   }
 }
 
-class _SearchState extends State<SearchWidget>{
-   final Color color = Colors.white;
- @override
- Widget build(BuildContext context) {
-   return Scaffold(
-     body: _buildBody(context),
-   );
- }
+class _SearchState extends State<SearchWidget> {
+  final TextEditingController _filter = new TextEditingController();
+  String _searchText = "";
+  List filteredNames = new List();
+  Icon _searchIcon = new Icon(Icons.search);
+  Widget _appBarTitle = new Text('Search Example');
 
- Widget _buildBody(BuildContext context) {
- return StreamBuilder<QuerySnapshot>(
-   stream: Firestore.instance.collection('items').snapshots(),
-   builder: (context, snapshot) {
-     if (!snapshot.hasData) return LinearProgressIndicator();
+  _SearchState() {
+    _filter.addListener(() {
+      if (_filter.text.isEmpty) {
+        setState(() {
+          _searchText = "";
+        });
+      } else {
+        setState(() {
+          _searchText = _filter.text;
+        });
+      }
+    });
+  }
 
-     return _buildList(context, snapshot.data.documents);
-   },
- );
-}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: _appBarTitle,
+        leading: new IconButton(
+          icon: _searchIcon,
+          onPressed: _searchPressed,
+        ),
+        backgroundColor: Colors.grey,
+      ),
+      body: _buildBody(context),
+    );
+  }
 
- Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-   return ListView(
-     padding: const EdgeInsets.only(top: 20.0),
-     children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-   );
- }
+  Widget _buildBody(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('items').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+        return _buildList(context, snapshot.data.documents);
+      },
+    );
+  }
 
- Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
- final record = Record.fromSnapshot(data);
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+    List filtered = new List<Widget>();
+    List items = new List<Widget>();
+    if ((_searchText.isNotEmpty)){
+      List tempList = new List();
+      filtered = snapshot.toList();
+      for (int i = 0; i < filtered.length; i++) {
+        final record = Record.fromSnapshot(filtered[i]);
+        if (record.name.toLowerCase().contains(_searchText.toLowerCase())) {
+          tempList.add(filtered[i]);
+        }
+      }
+      items = tempList.map((data) => _buildListItem(context, data)).toList();
+    }
+    else {
+      items = snapshot.map((data) => _buildListItem(context, data)).toList();
+    }
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: items,
+    );
+  }
 
-   return Padding(
-     key: ValueKey(record.name),
-     padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-     child: Container(
-       decoration: BoxDecoration(
-         border: Border.all(color: Colors.grey),
-         borderRadius: BorderRadius.circular(5.0),
-       ),
-       child: ListTile(
-         title: Text(record.name),
-         trailing: Text(record.likes.toString()),
-         onTap: () => record.reference.updateData({'likes': FieldValue.increment(1)}),
-       ),
-     ),
-   );
- }
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final record = Record.fromSnapshot(data);
+
+    return Padding(
+      key: ValueKey(record.name),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(record.name),
+          trailing: Text(record.likes.toString()),
+          onTap: () =>
+              record.reference.updateData({'likes': FieldValue.increment(1)}),
+        ),
+      ),
+    );
+  }
+
+  void _searchPressed() {
+    setState(() {
+      if (this._searchIcon.icon == Icons.search) {
+        this._searchIcon = new Icon(Icons.close);
+        this._appBarTitle = new TextField(
+          controller: _filter,
+          decoration: new InputDecoration(
+              prefixIcon: new Icon(Icons.search), hintText: 'Search...'),
+        );
+      } else {
+        this._searchIcon = new Icon(Icons.search);
+        this._appBarTitle = new Text('Search Example');
+        _filter.clear();
+      }
+    });
+  }
 }
 
 class Record {
- final String name;
- final int likes;
- final DocumentReference reference;
+  final String name;
+  final int likes;
+  final DocumentReference reference;
 
- Record.fromMap(Map<String, dynamic> map, {this.reference})
-     : assert(map['Name'] != null),
-       assert(map['likes'] != null),
-       name = map['Name'],
-       likes = map['likes'];
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['Name'] != null),
+        assert(map['likes'] != null),
+        name = map['Name'],
+        likes = map['likes'];
 
- Record.fromSnapshot(DocumentSnapshot snapshot)
-     : this.fromMap(snapshot.data, reference: snapshot.reference);
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
 
- @override
- String toString() => "Record<$name:$likes>";
+  @override
+  String toString() => "Record<$name:$likes>";
 }
